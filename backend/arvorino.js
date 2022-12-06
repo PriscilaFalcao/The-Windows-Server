98972f0a54e0e3a6766be647230308357d7f26e0
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2022-12-06 19:27:57 -0300

    teste

--- backend/arvorino.js
+++ backend/arvorino.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-var html = express.static("../frontend3/")
+var html = express.static("../frontend3/index.html")
 
 app.get("/", (req, res) => res.type('html').send(html));
 
