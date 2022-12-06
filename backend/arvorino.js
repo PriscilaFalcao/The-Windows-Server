fd899ce8eb335205a198338c13f1a8ca3d1a820b
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2022-12-06 16:05:58 -0300

    arvorino

--- backend/arvorino.js
+++ backend/arvorino.js
@@ -5,8 +5,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+var html = express.static("../frontend3/")
 
-app.get("/", (req, res) => res.type('html').send("../frontend3/"));
+app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
